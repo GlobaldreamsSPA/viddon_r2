49d8988f57e6a6aa456456edd44ab30fbb76dedb
Hugo Carrasco Diaz <ranca.hugo@gmail.com>
2012-10-29 16:45:30 -0300

    detalles del css arreglados y se agregan comentarios

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -10,19 +10,28 @@
 					?>
 				
 					<div id="main_videos" class='span4'>
-						<div class="space1"></div>
+						<div class="space2"></div>
 						<h4> <?php echo $video[0];?></h3>
-						<iframe width="370" height="200" src="http://www.youtube.com/embed/<?php echo $video[1]?>" frameborder="0" allowfullscreen></iframe>
+						<iframe width="410" height="260" src="http://www.youtube.com/embed/<?php echo $video[1]?>" frameborder="0" allowfullscreen></iframe>
 						<div class="space1"></div>				
 						<div class="fb-like" data-href="http://www.youtube.com/watch?v=<?php echo $video[1] ?>" data-send="true" data-layout="button_count" data-width="450" data-show-faces="true"></div>			    							
 						<label style="display:inline;">
 						<?php
 							$JSON = file_get_contents("https://gdata.youtube.com/feeds/api/videos/{$video[1]}?v=2&alt=json");
 							$JSON_Data = json_decode($JSON);
-							$views = $JSON_Data->{'entry'}->{'yt$statistics'}->{'viewCount'};
-							$dislikes = $JSON_Data->{'entry'}->{'yt$rating'}->{'numDislikes'};
-							$likes = $JSON_Data->{'entry'}->{'yt$rating'}->{'numLikes'};
-								
+							
+							if(array_key_exists('yt$statistics', $JSON_Data->{'entry'}))
+							{
+								$views = $JSON_Data->{'entry'}->{'yt$statistics'}->{'viewCount'};
+								$dislikes = $JSON_Data->{'entry'}->{'yt$rating'}->{'numDislikes'};
+								$likes = $JSON_Data->{'entry'}->{'yt$rating'}->{'numLikes'};
+							}
+							else
+							{
+								$views = "0";
+								$dislikes = "0";
+								$likes = "0";
+							}	
 							echo "<span id='youtubedata' class='badge badge-important'><i class='icon-thumbs-down icon-white'></i>$dislikes</span>";
 							echo "<span id='youtubedata' class='badge badge-success'><i class='icon-thumbs-up icon-white'></i>$likes</span>";
 							echo "<span id='youtubedata' class='badge badge-info'><i class='icon-eye-open '></i>$views</span>";
@@ -33,8 +42,10 @@
 			<?php if($i%3 == 0) echo "</div>"; }?>
 			
 			<div class="row-fluid">
+				<div class="space1"></div>
 				<div class="pagination">
-				  <ul>
+				  <ul id="pagination_bt">
+				  	<li class="disabled"><a href="#">back</a></li>
 				    <li class="disabled"><a href="#">1</a></li>
 					<li class="active"><a href="#">2</a></li>
 					<li class="active"><a href="#">3</a></li>
