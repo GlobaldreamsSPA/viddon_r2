6917d41874e5bb213d9acd3bb74d4a9d61eb447b
Matias <maty@mailinator.com>
2013-01-14 10:17:37 -0300

    Terminada la lista de castings

--- application/views/castings/list_view.php
+++ application/views/castings/list_view.php
@@ -10,8 +10,8 @@
 	    		<div class="span9 offset1">
 		    		<ul class="nav nav-pills nav-stacked orange">
 					  <li><a href="<?php echo HOME."/hunter";?>"> <i class="icon-user"></i> Perfil</a></li>
-					  <li><a href="#"> <i class="icon-pencil"></i> Nuevo Casting</a></li>
-					  <li class="active"><a href="#"> <i class="icon-edit"></i> Mis Castings</a></li>
+					  <li><a href="<?php echo HOME."/hunter/publish";?>"> <i class="icon-pencil"></i> Nuevo Casting</a></li>
+					  <li class="active"><a> <i class="icon-edit"></i> Mis Castings</a></li>
 					</ul>
 				</div>
 	    	</div>
@@ -20,84 +20,111 @@
 		    		
 		    	<div class="space1"></div>
 		    	<h1 class="profile-title"> Pedro Ramirez Soto
-		    	</h1> 
+		    	</h1>
 				<h2 id="profile">Area Dramatica Canal 13</h2>
 
-			
-				<div class="space1"></div>
 				<div class="row">
+					<div class="space1"></div>
 					<div class="span6">
-						<h3>Casting A Canal 13</h3>
-						<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
+						<h4 class="list-view-title">Casting A Canal 13</h4>
 					</div>
-					<div class="span6">
-						<div class="space4"></div>
-						<h4 id="profile">Descripci&oacuten</h4>
-						<div class="justify profile-content">
-							Se buscan Bandas con la pasi&oacuten para triunfar.
+					<div class="span5">
+						<h5 class="list-view-applies-count span2"><p>50</p></h5>
+						<h5 class="list-view-applies-text">Personas ya postularon</h5>
+					</div>
+					<div class="span11">
+						<div class="span7">
+							<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
 						</div>
 						<div class="space05"></div>
-						<h4 id="profile">Requisitos</h4>
-						<div class="justify profile-content">
-							Banda Pop, Rock, Punk.
+						<div class="span2">
+							<img id="list-view-logo" src=<? echo HOME."/img/canal-13.jpg"?> />
 						</div>
-						
-						<div class="space05"></div>
-						<h4 id="profile">Categor&iacutea</h4>
-						<div class="justify profile-content">
-							Musica - Grupo
+						<div class="span2 list-view-applies-desc">
+							<h4 id="profile">Descripci&oacuten:</h4>
+							<div class="justify profile-content">
+								Se buscan Bandas con la pasi&oacuten para triunfar.
+							</div>
+							<h4 id="profile">Requisitos:</h4>
+							<div class="justify profile-content">
+								Banda Pop, Rock, Punk.
+							</div>
+							<h4 id="profile">Categor&iacutea:</h4>
+							<div class="justify profile-content">
+								Musica - Grupo
+							</div>
+							<div id="time">Quedan 132 días</div>
 						</div>
 					</div>
 				</div>
 				<div class="row">
+					<div class="space1"></div>
 					<div class="span6">
-						<h3>Casting A Canal 13</h3>
-						<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
+						<h4 class="list-view-title">Casting A Canal 13</h4>
 					</div>
-					<div class="span6">
-						<div class="space4"></div>
-						<h4 id="profile">Descripci&oacuten</h4>
-						<div class="justify profile-content">
-							Se buscan Bandas con la pasi&oacuten para triunfar.
+					<div class="span5">
+						<h5 class="list-view-applies-count span2"><p>50</p></h5>
+						<h5 class="list-view-applies-text">Personas ya postularon</h5>
+					</div>
+					<div class="span11">
+						<div class="span7">
+							<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
 						</div>
 						<div class="space05"></div>
-						<h4 id="profile">Requisitos</h4>
-						<div class="justify profile-content">
-							Banda Pop, Rock, Punk.
+						<div class="span2">
+							<img id="list-view-logo" src=<? echo HOME."/img/canal-13.jpg"?> />
 						</div>
-						
-						<div class="space05"></div>
-						<h4 id="profile">Categor&iacutea</h4>
-						<div class="justify profile-content">
-							Musica - Grupo
+						<div class="span2 list-view-applies-desc">
+							<h4 id="profile">Descripci&oacuten:</h4>
+							<div class="justify profile-content">
+								Se buscan Bandas con la pasi&oacuten para triunfar.
+							</div>
+							<h4 id="profile">Requisitos:</h4>
+							<div class="justify profile-content">
+								Banda Pop, Rock, Punk.
+							</div>
+							<h4 id="profile">Categor&iacutea:</h4>
+							<div class="justify profile-content">
+								Musica - Grupo
+							</div>
+							<div id="time">Quedan 132 días</div>
 						</div>
 					</div>
 				</div>
 				<div class="row">
+					<div class="space1"></div>
 					<div class="span6">
-						<h3>Casting A Canal 13</h3>
-						<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
+						<h4 class="list-view-title">Casting A Canal 13</h4>
 					</div>
-					<div class="span6">
-						<div class="space4"></div>
-						<h4 id="profile">Descripci&oacuten</h4>
-						<div class="justify profile-content">
-							Se buscan Bandas con la pasi&oacuten para triunfar.
+					<div class="span5">
+						<h5 class="list-view-applies-count span2"><p>50</p></h5>
+						<h5 class="list-view-applies-text">Personas ya postularon</h5>
+					</div>
+					<div class="span11">
+						<div class="span7">
+							<img id="image_casting" src=<? echo HOME."/img/castings_dummy.png"?> />
 						</div>
 						<div class="space05"></div>
-						<h4 id="profile">Requisitos</h4>
-						<div class="justify profile-content">
-							Banda Pop, Rock, Punk.
+						<div class="span2">
+							<img id="list-view-logo" src=<? echo HOME."/img/canal-13.jpg"?> />
 						</div>
-						
-						<div class="space05"></div>
-						<h4 id="profile">Categor&iacutea</h4>
-						<div class="justify profile-content">
-							Musica - Grupo
+						<div class="span2 list-view-applies-desc">
+							<h4 id="profile">Descripci&oacuten:</h4>
+							<div class="justify profile-content">
+								Se buscan Bandas con la pasi&oacuten para triunfar.
+							</div>
+							<h4 id="profile">Requisitos:</h4>
+							<div class="justify profile-content">
+								Banda Pop, Rock, Punk.
+							</div>
+							<h4 id="profile">Categor&iacutea:</h4>
+							<div class="justify profile-content">
+								Musica - Grupo
+							</div>
+							<div id="time">Quedan 132 días</div>
 						</div>
 					</div>
 				</div>
-		
 			</div>
 			<div class="span2 user-profile-lateral">
 			</div>
--- application/views/castings/publish_view.php
+++ application/views/castings/publish_view.php
@@ -1,68 +1,46 @@
-<body>	
-	<div class="content" id="content">
-	
-		<div class="container-fluid">
-		  	<div class="row-fluid">	
-		  		<div class="space2"></div>
-				<!-- Imagen -->
-				<?php 
-				if (isset($update_values)) $flag="/update123";
-				else 
-					$flag = "";
-				
-				echo form_open_multipart('user/edit'.$flag); ?>
-				<div class="span3" id="image_upload">
-					<img src="<?php echo base_url(); ?>img/profile/<?php if(isset($update_values)) echo $update_values["image_profile"]; else echo "user.jpg"; ?>" class="img-polaroid">
-					<?php echo form_upload(array('name' => 'image_profile'));
-						  echo form_hidden('image','');
-						  echo form_error('image'); ?>
+<div class="content" id="content">
+	<div class="container-fluid">
+	  	<div class="row-fluid">
+	    	<div class="span3 user-profile-left">
+	    		<?php 
+	    			echo "<img class='user_image' src='".HOME."/img/profile_hunter/hunter_1.jpg'/>";
+	    		?>
+	    		<div class="space4"></div>
+	    		
+	    		<div class="span9 offset1">
+		    		<ul class="nav nav-pills nav-stacked orange">
+					  <li><a href="<?php echo HOME."/hunter";?>"> <i class="icon-user"></i> Perfil</a></li>
+					  <li><a href="<?php echo HOME."/hunter/casting_list";?>"> <i class="icon-pencil"></i> Nuevo Casting</a></li>
+					  <li class="active"><a> <i class="icon-edit"></i> Mis Castings</a></li>
+					</ul>
 				</div>
-				<!-- Texto -->
-				<div class="span9">
-					<h3>Quien eres</h3>
-					<input type="text" class="span7" placeholder="Escribe tu Nombre Aqui" value="<?php if(isset($update_values)) echo $update_values["name"]; else echo set_value('name');?>" name="name">
-					<?php echo form_error('name'); ?>
-					<h5>¿Cúal es tu sexo?</h5>
-					<select class="span2" name="sex">
-						<option value="0" <?php if(isset($update_values) && $update_values["sex"]==0) echo "selected='selected'";?>>Femenino</option>
-						<option value="1"  <?php if(isset($update_values) && $update_values["sex"]==1) echo "selected='selected'";?>>Masculino</option>
-					</select>
-					<h5>¿Y tu edad?</h5>
-						<?php
-						if(isset($update_values)) $age_set=$update_values["age"]; else $age_set=18; 
-						echo form_dropdown('age', $age, $age_set, "class='span2'") ?> <br/>
-					
-					<h3>Selecciona tus habilidades</h3>
-						<?php 
-						$skill_selected= array();
-						for ($i=0; $i<3; $i++)
-						{
-							if(isset($update_user_skills[$i]))
-								$skill_selected[$i]=$update_user_skills[$i];
-							else 
-								$skill_selected[$i]=0;
-								
-						}
-						echo form_dropdown('skills1', $skills, $skill_selected[0],"class='span2'"); 
-						echo form_dropdown('skills2', $skills, $skill_selected[1],"class='span2'"); 
-						echo form_dropdown('skills3', $skills, $skill_selected[2],"class='span2'"); 
-						?>
-					<h3>Bio</h3>
-						<textarea class="user_description" rows="4" span="7" placeholder="Cuéntanos sobre ti. Cómo eres y que haces." name="bio"><?php if(isset($update_values)) echo $update_values["bio"]; else echo set_value('bio');?></textarea>
-						<?php echo form_error('bio'); ?>
-					<h3>Hobbies</h3>
-						<textarea class="user_description" rows="4" span="7" placeholder="Háblanos sobre tus gustos y lo que te apasiona!" name="hobbies"><?php if(isset($update_values)) echo $update_values["hobbies"]; else echo set_value('hobbies');?></textarea>
-						<?php echo form_error('hobbies'); ?>
-					<h3>Mi Sueño</h3>
-						<textarea class="user_description" rows="4" span="7" placeholder="Cuéntanos de tus sueños y lo que quieres lograr!" name="dreams"><?php if(isset($update_values)) echo $update_values["dreams"]; else echo set_value('dreams');?></textarea>
-						<?php echo form_error('dreams'); ?>
-					<div class="space2"></div>
-					<input class="btn btn-primary" type="submit" value="Guardar Datos" />
-					</form>
-					<div class="space4"></div>
-				</div>
+	    	</div>
+		    
+		    <div class="span6 user-profile-right">
+		    		
+		    	<div class="space1"></div>
+		    	<h1 class="profile-title"> Pedro Ramirez Soto
+		    	</h1> 
+				<h2 id="profile">Area Dramatica Canal 13</h2>
+
+			
+				<div class="space1"></div>
+
+		
 			</div>
-			<div class="space2"></div>
+			<div class="span2 user-profile-lateral">
+			</div>
 		</div>
-	</div>
- </body>
+		
+		<div class="row-fluid">	
+			<div class="space4"></div>	
+		</div>
+		
+		<div class="row-fluid">
+			<div class="span7 offset3 user-profile-right">
+							
+			</div>
+		</div>
+  	</div>
+  	<div class="space2"></div> 	
+</div>
