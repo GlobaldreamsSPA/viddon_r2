99b27a9bb373a43cb907148e844cc8c54ba3718a
Matias Lecaros <m.lecaros.aranis@gmail.com>
2013-04-02 11:57:39 -0400

    Pequenia Correccion de texto "whatis".

--- application/views/home/what_is.php
+++ application/views/home/what_is.php
@@ -12,10 +12,10 @@
 								<h1>&iquest;QU&Eacute; ES VIDDON?</h1>
 								<p>
 									www.viddon.com es una plataforma que pretende potenciar artistas emergentes de toda Latino&aacute;rica
-									y posteriormente del mundo entero. En Viddon, podr&aacute;s subir&aacute;n videos propios a la plataforma, 
-									y con esos videos, podr&aacute;n postular a distintas ofertas laborales (casting) en Chile o en otros pa&iacute;ses, 
+									y posteriormente del mundo entero. En Viddon, podr&aacute;s subir videos propios a la plataforma, 
+									y con esos videos, podr&aacute;n postular a distintas ofertas laborales ("casting") en Chile o en otros pa&iacute;ses, 
 									dandote la posibilidad de que por ejemplo, si eres cantante, no viajes a otra region u otro pais para una 
-									audici&oacute;n, si no que realice dicha audici&oacute;n a traves de un video, de forma que ahorres en costos econ&oacute;micos 
+									audici&oacute;n, si no que se realice dicha audici&oacute;n a traves de un video, de forma que ahorres en costos econ&oacute;micos 
 									y de tiempo. Viddon lo que busca es hacer el "puente" entre distintos artistas, con ofertas laborales en 
 									sus respectivas areas y en distintos pa&iacute;ses, de manera de potenciar sus posibilidades de &eacute;xito en cada una 
 									de sus industrias, y que su talento sea visto de manera global, todo a traves de sus propios videos.
